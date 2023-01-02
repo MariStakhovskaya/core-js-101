ae235a026eb65149b485999a3fbc81434e1fa068
Mari Stakhovskaya <maryia.jegorova@gmail.com>
2023-01-03 01:53:56 +0300

    feat: resolve getDigitalRoot

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -290,8 +290,13 @@ function isCreditCardNumber(/* ccn */) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const arr = String(num).split('');
+  let sum = 0;
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += +arr[i];
+  }
+  return sum > 9 ? getDigitalRoot(sum) : sum;
 }
 
 
